abe5974af299d86b31c046ad61f3599341bc4efd
Ghorbali Issam <issam.ghorbali@gmail.com>
2021-12-10 10:57:35 +0100

    test main

---
+++ vendor/composer/installed.php
@@ -0,0 +1,35 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '086468417b6adce5bfd477cf16a91ef4e1ab1cfa',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '086468417b6adce5bfd477cf16a91ef4e1ab1cfa',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'pretty_version' => 'v5.1.3',
+            'version' => '5.1.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twitter/bootstrap',
+            'aliases' => array(),
+            'reference' => '1a6fdfae6be09b09eaced8f0e442ca6f7680a61e',
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.1.3',
+            ),
+        ),
+    ),
+);
